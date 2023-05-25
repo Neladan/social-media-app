abbf661245866a01f329fc7487af57afeabf2731
Lionel ADANKON <lionelad006@gmail.com>
2023-05-25 17:18:16 +0100

    Post Routes Setup

--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
 
 
@@ -49,6 +50,7 @@ const upload = multer({ storage });
 
 /* ROUTES WITH FILES*/
 app.post("/auth/register", upload.single("picture"), verifyToken, register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 
 /* ROUTES */
